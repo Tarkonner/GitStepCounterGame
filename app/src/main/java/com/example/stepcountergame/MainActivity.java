aaca1298e60a57dbfa308a70741f488bbf6df5b1
Tarkonner <41646766+Tarkonner@users.noreply.github.com>
2020-04-29 13:15:03 +0200

    Projekt

---
+++ app/src/main/java/com/example/stepcountergame/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.stepcountergame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Bundle;
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private TextView steps;
+    private Button upgrade;
+    private SensorManager sensor;
+
+    public int score = 0;
+
+    private float test = 22;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        steps = (TextView) findViewById(R.id.Steps);
+        upgrade = (Button) findViewById(R.id.Upgrade);
+        sensor = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+
+
+        steps.setText(String.valueOf(score));
+
+        upgrade.setOnClickListener(new View.OnClickListener()
+        {
+           @Override
+           public  void onClick(View v)
+           {
+                updateScore();
+           }
+        });
+    }
+
+    void updateScore()
+    {
+        score++;
+        steps.setText(String.valueOf(score));
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
+
+
---
+++ app/src/main/java/com/example/stepcountergame/StepListener.java
@@ -0,0 +1,5 @@
+package com.example.stepcountergame;
+
+public interface StepListener {
+    public void step(long timeNs);
+}
