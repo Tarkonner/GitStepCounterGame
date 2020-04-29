61df9df4ba170d2120c8b276d323c407d48d46dc
Tarkonner <41646766+Tarkonner@users.noreply.github.com>
2020-04-29 15:26:43 +0200

    Base system

--- app/src/main/java/com/example/stepcountergame/MainActivity.java
+++ app/src/main/java/com/example/stepcountergame/MainActivity.java
@@ -21,12 +21,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private TextView steps;
     private Button upgrade;
+    private Button click;
+    private TextView multiText;
+    private TextView upgradeText;
 
     public int score = 0;
 
     private SensorManager sensorManager;
     private Sensor sensor;
 
+    private  int pointsToGive = 1;
+
+    private int costPerMultiply = 30;
 
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -36,30 +42,63 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Setup
         steps = (TextView) findViewById(R.id.Steps);
+        multiText = (TextView) findViewById(R.id.Multi);
+        upgradeText = (TextView) findViewById(R.id.UpgradeText);
         upgrade = (Button) findViewById(R.id.Upgrade);
+        click = (Button) findViewById(R.id.Click);
 
+
+        //Sensor
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
 
+        //Show start stats
+        upgradeText.setText(String.valueOf(pointsToGive * costPerMultiply));
+        multiText.setText("Multiply: " + String.valueOf(pointsToGive));
         steps.setText(String.valueOf(score));
 
-        score = sensor.getFifoReservedEventCount();
+        
 
-        upgrade.setOnClickListener(new View.OnClickListener()
+        click.setOnClickListener(new View.OnClickListener()
         {
            @Override
            public  void onClick(View v)
            {
-                updateScore();
+               updateScore();
            }
         });
+
+        upgrade.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public  void onClick(View v)
+            {
+                pointsUpgrade();
+            }
+        });
+    }
+
+    void pointsUpgrade()
+    {
+        int cost = pointsToGive * costPerMultiply;
+
+        if(score >= cost)
+        {
+            score -= cost;
+            pointsToGive++;
+            steps.setText(String.valueOf(score));
+            multiText.setText("Multiply: " + String.valueOf(pointsToGive));
+
+            int newCost = pointsToGive * costPerMultiply;
+            upgradeText.setText(String.valueOf(newCost));
+        }
     }
 
     void updateScore()
     {
-        score++;
+        score += pointsToGive;
         steps.setText(String.valueOf(score));
     }
 
