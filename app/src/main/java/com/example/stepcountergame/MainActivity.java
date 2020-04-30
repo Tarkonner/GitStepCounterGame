e15f9529cd0633ff8071c3af2637fcf2556fd909
Tarkonner <41646766+Tarkonner@users.noreply.github.com>
2020-04-30 20:06:53 +0200

    MPV
    en MPV hvor vi har base gameplay loopet sat op, og fået en form for stepcounter

--- app/src/main/java/com/example/stepcountergame/MainActivity.java
+++ app/src/main/java/com/example/stepcountergame/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.stepcountergame;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -11,28 +10,29 @@ import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
-import android.os.Bundle;
-import org.w3c.dom.Text;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private TextView steps;
     private Button upgrade;
-    private Button click;
     private TextView multiText;
     private TextView upgradeText;
 
-    public int score = 0;
+
 
     private SensorManager sensorManager;
     private Sensor sensor;
+    private boolean runnig = false;
 
+    //Variabler to change
+    public int score = 0;
     private  int pointsToGive = 1;
-
     private int costPerMultiply = 30;
+
+
 
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -47,28 +47,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         multiText = (TextView) findViewById(R.id.Multi);
         upgradeText = (TextView) findViewById(R.id.UpgradeText);
         upgrade = (Button) findViewById(R.id.Upgrade);
-        click = (Button) findViewById(R.id.Click);
 
 
         //Sensor
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        assert sensorManager != null;
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
 
         //Show start stats
         upgradeText.setText(String.valueOf(pointsToGive * costPerMultiply));
-        multiText.setText("Multiply: " + String.valueOf(pointsToGive));
+        multiText.setText("Multiply: " + pointsToGive);
         steps.setText(String.valueOf(score));
 
-        
+        //Start the Step Counter
+        onResume();
 
-        click.setOnClickListener(new View.OnClickListener()
-        {
-           @Override
-           public  void onClick(View v)
-           {
-               updateScore();
-           }
-        });
 
         upgrade.setOnClickListener(new View.OnClickListener()
         {
@@ -89,28 +82,47 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             score -= cost;
             pointsToGive++;
             steps.setText(String.valueOf(score));
-            multiText.setText("Multiply: " + String.valueOf(pointsToGive));
+            multiText.setText("Multiply: " + pointsToGive);
 
             int newCost = pointsToGive * costPerMultiply;
             upgradeText.setText(String.valueOf(newCost));
         }
     }
 
-    void updateScore()
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    protected void onResume()
     {
-        score += pointsToGive;
-        steps.setText(String.valueOf(score));
+        super.onResume();
+        runnig = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(countSensor != null)
+        {
+            sensorManager.registerListener(this, countSensor, sensorManager.SENSOR_DELAY_UI);
+        }
+        else
+        {
+            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void onPause(){
+        super.onPause();
+        runnig = false;
+        sensorManager.unregisterListener(this);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        updateScore();
+        if(runnig)
+        {
+            score += pointsToGive;
+            //steps.setText(String.valueOf(event.values[0]));
+            steps.setText(String.valueOf(score));
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        updateScore();
     }
 }
-
 
